803aa204526f5c8a2a7608e7911baa80f0df9f8f
Krawller <csharpdomingos@gmail.com>
2018-03-15 09:26:04 +0000

    First Commit
    Upload of XPathTester - working version

---
+++ XPathTester/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace XPathTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbxURL = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.tbxExpression = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.lblNodeCount = new System.Windows.Forms.Label();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.tbxValue = new System.Windows.Forms.TextBox();
+			this.tbxUserAgent = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.tbxCookie = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "URL:";
+			// 
+			// tbxURL
+			// 
+			this.tbxURL.Location = new System.Drawing.Point(51, 10);
+			this.tbxURL.Name = "tbxURL";
+			this.tbxURL.Size = new System.Drawing.Size(479, 20);
+			this.tbxURL.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Xpath:";
+			// 
+			// tbxExpression
+			// 
+			this.tbxExpression.Location = new System.Drawing.Point(51, 36);
+			this.tbxExpression.Name = "tbxExpression";
+			this.tbxExpression.Size = new System.Drawing.Size(479, 20);
+			this.tbxExpression.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 150);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(72, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Node Counts:";
+			// 
+			// lblNodeCount
+			// 
+			this.lblNodeCount.AutoSize = true;
+			this.lblNodeCount.Location = new System.Drawing.Point(91, 150);
+			this.lblNodeCount.Name = "lblNodeCount";
+			this.lblNodeCount.Size = new System.Drawing.Size(13, 13);
+			this.lblNodeCount.TabIndex = 5;
+			this.lblNodeCount.Text = "0";
+			// 
+			// btnGo
+			// 
+			this.btnGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnGo.Location = new System.Drawing.Point(455, 154);
+			this.btnGo.Name = "btnGo";
+			this.btnGo.Size = new System.Drawing.Size(75, 23);
+			this.btnGo.TabIndex = 8;
+			this.btnGo.Text = "Get Content";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(14, 167);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(37, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Value:";
+			// 
+			// tbxValue
+			// 
+			this.tbxValue.Location = new System.Drawing.Point(12, 183);
+			this.tbxValue.Multiline = true;
+			this.tbxValue.Name = "tbxValue";
+			this.tbxValue.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbxValue.Size = new System.Drawing.Size(518, 120);
+			this.tbxValue.TabIndex = 10;
+			// 
+			// tbxUserAgent
+			// 
+			this.tbxUserAgent.Location = new System.Drawing.Point(82, 62);
+			this.tbxUserAgent.Name = "tbxUserAgent";
+			this.tbxUserAgent.Size = new System.Drawing.Size(448, 20);
+			this.tbxUserAgent.TabIndex = 12;
+			this.tbxUserAgent.Text = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome" +
+    "/61.0.3163.49 Safari/537.36 OPR/48.0.2685.7 (Edition beta)";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(13, 65);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(63, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "User Agent:";
+			// 
+			// tbxCookie
+			// 
+			this.tbxCookie.Location = new System.Drawing.Point(62, 88);
+			this.tbxCookie.Name = "tbxCookie";
+			this.tbxCookie.Size = new System.Drawing.Size(468, 20);
+			this.tbxCookie.TabIndex = 14;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(13, 91);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(43, 13);
+			this.label6.TabIndex = 13;
+			this.label6.Text = "Cookie:";
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.btnGo;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(542, 312);
+			this.Controls.Add(this.tbxCookie);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.tbxUserAgent);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.tbxValue);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.btnGo);
+			this.Controls.Add(this.lblNodeCount);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.tbxExpression);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.tbxURL);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.Text = "XPathTester";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxExpression;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNodeCount;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxValue;
+		private System.Windows.Forms.TextBox tbxUserAgent;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox tbxCookie;
+		private System.Windows.Forms.Label label6;
+	}
+}
+
---
+++ XPathTester/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Xml.XPath;
+using System.Xml;
+
+namespace XPathTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+
+			if(string.IsNullOrEmpty(tbxUserAgent.Text))
+			{ 
+				wc.Headers.Add("user-agent", tbxUserAgent.Text);
+			}
+
+			if (string.IsNullOrEmpty(tbxCookie.Text))
+			{
+				//"ai_user=7OSii|2017-08-14T21:24:32.811Z; Slb_CookiePolicy=true; benficadp#lang=pt-PT; ASP.NET_SessionId=a3jw3uby40irkhbmgxmkmixg; ARRAffinity=0c1893ceb7315936dd8dce5382062c1695bae0bb25a74cbe99fae7245da3c4e7; TS01181c65=017e78a9ff5bc9bf3a4766e4d8f001b7fee1147a9ae5d766509122e3876b6fa9ed3ba4955a6701dd48be745265fe2b8c81e74698f8d0e21aa1fb099b046e56a93a93b352ba; visid_incap_1301582=Qsqu9adETd6MyhwSJh4tJVytlVkAAAAAQUIPAAAAAADUu85qoY+yXwTgSmZsYd/y; incap_ses_457_1301582=5RWaDQvmdVrsaHbZtJdXBkiJplkAAAAA0bn6Ob+BRXC42zX0e7qQMw==; _ga=GA1.2.1579848954.1502745873; _gid=GA1.2.564709534.1504085912; ai_session=cghpR|1504085912717|1504086005335.84; TSc5941052_27=08da0952caab20006242f5f876df25b0de08b9e52be2fed1279e49052ff1c13627a4eb89ceab1ab3088759f0721120001f09fb2d1810234df50a0340bfeea19d524256f67c06c683b628360df8646dfc; TS01f32fae=010716811d0d867638e99e83c4759477c940831f6b729add8876fae0dd27db307f21e1d38b93592608680f51864af3b01566292a9e"
+				wc.Headers.Add("Cookie", tbxCookie.Text);
+			}
+
+			try
+            {
+                string html = wc.DownloadString(tbxURL.Text);
+
+				if(html.StartsWith("<?xml"))
+				{
+					XmlDocument doc = new XmlDocument();
+					doc.LoadXml(html);
+
+					int count = doc.SelectNodes(tbxExpression.Text).Count;
+					lblNodeCount.Text = count.ToString();
+
+					XPathNavigator nav = doc.CreateNavigator();
+					tbxValue.Text = nav.SelectSingleNode(tbxExpression.Text).Value;
+				}
+				else
+				{ 
+					HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+					doc.LoadHtml(html);
+
+					int count = doc.DocumentNode.SelectNodes(tbxExpression.Text).Count;
+					lblNodeCount.Text = count.ToString();
+
+					XPathNavigator nav = doc.DocumentNode.CreateNavigator();
+					tbxValue.Text = nav.SelectSingleNode(tbxExpression.Text).Value;
+				}
+			}
+            catch (Exception ex)
+            {
+                tbxValue.Text = "==/No Value\\==" + Environment.NewLine +  "Exception: " + ex.Message;
+            }
+        }
+    }
+}
